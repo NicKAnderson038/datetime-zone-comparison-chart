8fd770c0f614d17255680969ae80f33e1ce03916
NicKAnderson038 <nick7anderson@protonmail.com>
2023-04-17 13:26:06 -0700

    🗑️ clean up

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/datetime-zone-comparison-chart/",
   build: {
     outDir: './docs', // relative to index.html
     // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
